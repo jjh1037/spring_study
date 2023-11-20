115aab50d206098c63bc416a0d0ffe249b59e237
jjh1037 <thwibe1037@gmail.com>
2023-11-20 16:43:57 +0900

    Add files via upload

---
+++ sel/src/main/java/com/example/sel/dto/DeptDto.java
@@ -0,0 +1,22 @@
+package com.example.sel.dto;
+
+public class DeptDto {
+    private String deptCode;
+    private String deptName;
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
---
+++ sel/src/main/java/com/example/sel/dto/PosDto.java
@@ -0,0 +1,32 @@
+package com.example.sel.dto;
+
+
+public class PosDto {
+    private String posCode;
+    private String posName;
+    private String deptCode;
+
+    public String getPosCode() {
+        return posCode;
+    }
+
+    public void setPosCode(String posCode) {
+        this.posCode = posCode;
+    }
+
+    public String getPosName() {
+        return posName;
+    }
+
+    public void setPosName(String posName) {
+        this.posName = posName;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
+    }
+}
