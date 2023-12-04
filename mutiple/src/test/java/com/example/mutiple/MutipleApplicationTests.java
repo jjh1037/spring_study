505928b6a393eba7d1bcee67f0e03db3410d1b5b
jjh1037 <thwibe1037@gmail.com>
2023-12-04 16:49:27 +0900

    Add files via upload

---
+++ mutiple/src/test/java/com/example/mutiple/MutipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutiple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
